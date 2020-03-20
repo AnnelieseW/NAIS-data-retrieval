a8150c28c5de6ab5a7df7aed7d936f08c9ab785f
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2020-03-20 12:58:00 -0400

    adapted for multiple directories

--- dataExtraction.py
+++ dataExtraction.py
@@ -8,29 +8,30 @@ import xlsxwriter
 
 print(os.getcwd())
 shp_filepath = "/Users/anniewen/Documents/Dev/NAIS/EasternOntarioCAR_10kgrid_corn/10kecar_polygon_CornSubset.shp" # path to shape file
-tif_dirpath="Temperature" # Path to folder containing tifs of interest
+tif_dirpath=["Temperature", "RDPA" ] # List of paths to folder containing tifs of interest
+d = {}
 output_dir = " "
 
-lst_files = os.listdir(tif_dirpath)
-
-data = gpd.read_file(shp_filepath)
-newdf = data['ID']
-df_x = newdf.to_frame()
-
-count = 0
-for tif in lst_files:
-    count += 1
-
-    stats = zonal_stats(shp_filepath, os.path.join(tif_dirpath,tif))
-    mean_lst = [f['mean'] for f in stats]
-    np_mean_list = np.asarray(mean_lst)
-
-    df_x.insert(count, tif, np_mean_list)
-
-print(df_x)
-print(os.getcwd())
-
 writer = pd.ExcelWriter("Extracted_Values.xlsx", engine='xlsxwriter')
-df_x.to_excel(writer, sheet_name="replace")
 
+for dir in tif_dirpath:
+    lst_files = os.listdir(dir)
+
+    data = gpd.read_file(shp_filepath)
+    newdf = data['ID']
+    df_x = newdf.to_frame()
+
+    count = 0
+    for tif in lst_files:
+        count += 1
+
+        stats = zonal_stats(shp_filepath, os.path.join(dir,tif))
+        mean_lst = [f['mean'] for f in stats]
+        np_mean_list = np.asarray(mean_lst)
+
+        df_x.insert(count, tif, np_mean_list)
+    # d["{0}".format(dir)] = df_x
+    df_x.to_excel(writer, sheet_name=dir)
+
+print(os.getcwd())
 writer.save()
